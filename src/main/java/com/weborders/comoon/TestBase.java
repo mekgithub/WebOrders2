894fa709ea89f06caafc134abcc2ca63de66562a
mekgithub <mkortak1970@gmail.com>
2020-01-12 14:32:32 -0600

    weborders 12 jan

---
+++ src/main/java/com/weborders/comoon/TestBase.java
@@ -0,0 +1,28 @@
+package com.weborders.comoon;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+    @BeforeSuite
+    public void beforeSuite(){
+        Driver.setUpDriver();
+
+    }
+    @BeforeMethod
+    public void beforeMethod(){
+        Driver.getDriver();
+
+    }
+    @AfterMethod
+    public void afterMethod(){
+        Driver.closeDriver();
+
+    }
+    @AfterSuite
+    public void afterSuite(){
+
+    }
+}
